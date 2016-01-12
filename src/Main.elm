30dbec3a9fbca093028d53321cf69255074ad427
Peter Anderson <anderson.peter@byu.edu>
2016-01-12 16:53:22 -0700

    Fixed size for buttons

--- src/Main.elm
+++ src/Main.elm
@@ -178,8 +178,8 @@ pageButton address action disabled' label =
             [ onClick address action
             , disabled disabled'
             , style
-                [ ("width", "25%")
-                , ("height", "10%")
+                [ ("width", "225px")
+                , ("height", "50px")
                 , ("font-family", "Helvetica")
                 , ("font-size", "40px")
                 , ("font-weigth", "400")
