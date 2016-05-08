c2491bb87586abf5064bad35ad56049018250079
Luke Westby <lwestby@alumni.nd.edu>
2016-05-08 15:55:10 -0500

    update to 0.17

---
+++ src/Cmds.elm
@@ -0,0 +1,29 @@
+port module Cmds exposing (loadProjects, notifyOffsetChanged)
+
+-- where
+
+import Json.Decode as Decode
+import Task
+import HttpBuilder exposing (jsonReader, stringReader)
+import Model exposing (Project, decodeProject)
+
+
+projectsReader : HttpBuilder.BodyReader (List Project)
+projectsReader =
+  jsonReader <| Decode.list decodeProject
+
+
+port notifyOffsetChangedRaw : () -> Cmd msg
+
+
+notifyOffsetChanged : Cmd msg
+notifyOffsetChanged =
+  notifyOffsetChangedRaw ()
+
+
+loadProjects : msg -> (List Project -> msg) -> Cmd msg
+loadProjects failure success =
+  HttpBuilder.get "data/projects.json"
+    |> HttpBuilder.send projectsReader stringReader
+    |> Task.map .data
+    |> Task.perform (always failure) success
---
+++ src/Update.elm
@@ -0,0 +1,62 @@
+module Update exposing (update, Msg(..), initialize)
+
+-- where
+
+import Model exposing (Model, Project)
+import Cmds exposing (loadProjects, notifyOffsetChanged)
+
+
+type Msg
+  = NoOp
+  | Prev
+  | Next
+  | LoadProjectsSuccess (List Project)
+  | LoadProjectsError
+
+
+initialize : Cmd Msg
+initialize =
+  loadProjects LoadProjectsError LoadProjectsSuccess
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    NoOp ->
+      ( model, Cmd.none )
+
+    Prev ->
+      if model.offset - model.limit >= 0 then
+        ( { model
+            | offset = model.offset - model.limit
+          }
+        , notifyOffsetChanged
+        )
+      else
+        ( model, Cmd.none )
+
+    Next ->
+      if model.offset + model.limit < List.length model.projects then
+        ( { model
+            | offset = model.offset + model.limit
+          }
+        , notifyOffsetChanged
+        )
+      else
+        ( model, Cmd.none )
+
+    LoadProjectsSuccess projects ->
+      ( { model
+          | projects = projects
+          , isLoading = False
+        }
+      , Cmd.none
+      )
+
+    LoadProjectsError ->
+      ( { model
+          | loadFailed = True
+          , isLoading = False
+        }
+      , Cmd.none
+      )
