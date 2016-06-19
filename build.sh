2b7d37f3525dda61edb986ce0b0dbc829bc7f421
Luke Westby <lwestby@alumni.nd.edu>
2016-06-19 17:04:30 -0500

    optimizations to reduce filesize

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 elm-css src/Stylesheets.elm
 elm-make src/Main.elm --output build/main.js
+uglifyjs ./build/main.js --output ./build/main.js --screw-ie8 --compress dead_code,pure_getters,negate_iife,cascade,hoist_funs --mangle toplevel=true 
